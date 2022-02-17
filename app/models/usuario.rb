c874609cd66d12bee4dd22d9b2b83f12398d92db
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-02-16 22:19:51 -0300

    Configurado login com CPF e perfis.

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,5 +2,23 @@ class Usuario < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:cpf]
+
+  validates_uniqueness_of :cpf
+  validates_presence_of :nome, :cpf, :email
+  
+  VISUALIZADOR = 'Visualizador'
+  PROFESSOR = 'Professor'
+  COORDENADOR = 'Coordenador de Curso'
+  DIRETOR = 'Diretor'
+  GESTOR_TIC = 'Gestor de TIC'
+
+  PERFIL = [
+    VISUALIZADOR,
+    PROFESSOR,
+    COORDENADOR,
+    DIRETOR,
+    GESTOR_TIC
+  ]        
+
 end
