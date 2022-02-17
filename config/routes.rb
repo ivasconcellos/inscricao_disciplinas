7f4520907b6ce381f272edce1778bdb75b6cb871
Isadora Vasconcellos <ivasconcellos@iff.edu.br>
2022-02-16 22:17:34 -0300

    Gerado Devise de usuário.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
   resources :cursos
   resources :locais
   devise_for :admins
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_010753) do
+ActiveRecord::Schema.define(version: 2022_02_17_011550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,23 @@ ActiveRecord::Schema.define(version: 2022_02_17_010753) do
     t.boolean "ativo", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string "nome", default: "", null: false
+    t.string "cpf", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "telefone"
+    t.string "encrypted_password", default: "", null: false
+    t.string "perfil", default: "Visualizador", null: false
+    t.boolean "ativo", default: true
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_usuarios_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
   add_foreign_key "cursos", "locais"
